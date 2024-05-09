931773c5c9d334b5b8eb44887b4c4888e80f1775
TasnovaNihat-15 <ramisakhan418@gmail.com>
2024-05-09 14:57:07 +0600

    changed

--- Task_101.cpp
+++ Task_101.cpp
@@ -1,130 +1,88 @@
-#include<stdio.h>
-
-#include<SDL2/SDL.h>
-
-#include<math.h>
-
-int SCREEN_WIDTH = 640;
-
-int SCREEN_HEIGHT = 640;
-
-SDL_Window* window = NULL;
-
-SDL_Renderer* renderer = NULL;
-
-bool game_running = false;
-
-int radius = 20;
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <math.h>
+#define SCREEN_WIDTH 500
+#define SCREEN_HEIGHT 500
 
 int CENTERX = SCREEN_WIDTH / 2;
+int CENTERY = SCREEN_WIDTH / 2;
+int radius = 80;
 
-int CENTERY = SCREEN_HEIGHT / 2;
-
-bool activate_window()
+void MakeSolidCircle(SDL_Renderer* renderer, int centerX, int centerY, int rad)
 {
-    if (SDL_Init(SDL_INIT_VIDEO) != NULL)
+    for (int x = -rad; x <= rad; x++)
     {
-        printf("ERROR INITIALIZING SDL!");
-        return false;
-    }
-
-    window = SDL_CreateWindow("MADE BY TASNOVA", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-
-    if (!window)
-    {
-        return false;
-    }
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    if (!renderer)
-    {
-        return false;
-
-
-        
-    }
-
-    return true;
-}
-
-void startup()
-{
-    SDL_SetRenderDrawColor(renderer, 0, 0, 70, 200);
-
-    SDL_RenderClear(renderer);
-
-    SDL_Color color = { 200,0,0,200 };
-
-    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
-
-}
-
-void MakeCircle(SDL_Renderer* renderer, int cx, int cy, int radius)
-{
-    for (int x = -radius; x <= radius; x++)
-    {
-        for (int y = -radius; y <= radius; y++)
+        for (int y = -rad; y <= rad; y++)
         {
-            if (x * x + y * y <= radius * radius)
+            if (x * x + y * y <= rad * rad)
             {
-                SDL_RenderDrawPoint(renderer, cx + x, cy + y);
+                SDL_RenderDrawPoint(renderer, centerX + x, centerY + y);
             }
         }
     }
 }
-
-void update()
+bool activateSDL(SDL_Window** window, SDL_Renderer** renderer)
 {
-    radius += 5;
-
-    if (radius >= 320)
+    if (SDL_Init(SDL_INIT_VIDEO) != NULL)
     {
-        radius = 20;
+        printf("ERROR INITIALIZING SDL!");
+
+        return false;
     }
 
-    SDL_RenderClear(renderer);
+    *window = SDL_CreateWindow("MADE BY TASNOVA", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-    SDL_SetRenderDrawColor(renderer, 100, 110, 0, 255);
+    if (*window == NULL)
+    {
+        printf("ERROR INITIALIZING SDL!");
 
-    SDL_RenderClear(renderer);
+        return false;
+    }
 
-    SDL_SetRenderDrawColor(renderer, 100, 0, 0, 200);
+    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    if (*renderer == 0)
+    {
+        printf("ERROR INITIALIZING RENDERER!");
+
+        return false;
+    }
+
+    return true;
 }
-
-void render()
-{
-    MakeCircle(renderer, CENTERX, CENTERY, radius);
-
-    SDL_RenderPresent(renderer);
-}
-
-void destroyWindow()
-{
-    SDL_DestroyRenderer(renderer);
-
-    SDL_DestroyWindow(window);
-
-    SDL_Quit();
-}
-
 int main(int argc, char* argv[])
 {
-    game_running = activate_window();
-  SDL_Event windowevent;
-    while (game_running != false)
-    {
-        update();
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
 
-        render();
-         if(SDL_PollEvent(&windowevent))
-        {
-            if(SDL_QUIT==windowevent.type)
-                break;
-        }
+    if (!activateSDL(&window, &renderer))
+    {
+        return 1;
     }
 
-    destroyWindow();
+    SDL_Event event;
+
+    bool running = true;
+
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 150, 200, 255, 100);
+        SDL_RenderClear(renderer);
+
+
+        SDL_SetRenderDrawColor(renderer, 100, 0, 0, 100);
+        MakeSolidCircle(renderer, CENTERX, CENTERY, radius);
+
+        SDL_RenderPresent(renderer);
+    }
 
     return 0;
 }
--- Task_102.cpp
+++ Task_102.cpp
@@ -121,7 +121,7 @@ int main(int argc, char* argv[])
         }
     }
 
-    destroyWindow();
+
 
     return 0;
 }
